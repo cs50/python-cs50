ddc93eb677c149822bbc1e0763273e9b97e3350f
David J. Malan <malan@harvard.edu>
2016-10-26 23:55:30 -0400

    added additional exception handling

--- src/sql.py
+++ src/sql.py
@@ -6,30 +6,36 @@ class SQL(object):
 
     def __init__(self, url):
         """TODO"""
-        self.engine = sqlalchemy.create_engine(url)
-
-    def execute(self, text, *multiparams, **params):
-        """TODO"""
-
-        # http://docs.sqlalchemy.org/en/latest/core/sqlelement.html#sqlalchemy.sql.expression.text
-        # https://groups.google.com/forum/#!topic/sqlalchemy/FfLwKT1yQlg
-        # http://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.Engine.execute
-        # http://docs.sqlalchemy.org/en/latest/faq/sqlexpressions.html#how-do-i-render-sql-expressions-as-strings-possibly-with-bound-parameters-inlined
         try:
-            statement = sqlalchemy.text(text).bindparams(*multiparams, **params)
-            result = self.engine.execute(str(statement.compile(compile_kwargs={"literal_binds": True})))
+            self.engine = sqlalchemy.create_engine(url)
         except Exception as e:
             raise RuntimeError(e)
 
-        # SELECT
-        if result.returns_rows:
-            rows = result.fetchall()
-            return [dict(row) for row in rows]
+    def execute(self, text, *multiparams, **params):
+        """TODO"""
+        try:
 
-        # INSERT
-        elif result.lastrowid is not None:
-            return result.lastrowid
+            # http://docs.sqlalchemy.org/en/latest/core/sqlelement.html#sqlalchemy.sql.expression.text
+            # https://groups.google.com/forum/#!topic/sqlalchemy/FfLwKT1yQlg
+            # http://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.Engine.execute
+            # http://docs.sqlalchemy.org/en/latest/faq/sqlexpressions.html#how-do-i-render-sql-expressions-as-strings-possibly-with-bound-parameters-inlined
+            statement = sqlalchemy.text(text).bindparams(*multiparams, **params)
+            result = self.engine.execute(str(statement.compile(compile_kwargs={"literal_binds": True})))
 
-        # DELETE, UPDATE
-        else:
-            return result.rowcount
+            # SELECT
+            if result.returns_rows:
+                rows = result.fetchall()
+                return [dict(row) for row in rows]
+
+            # INSERT
+            elif result.lastrowid is not None:
+                return result.lastrowid
+
+            # DELETE, UPDATE
+            else:
+                return result.rowcount
+
+        except Exception as e:
+            raise RuntimeError(e)
+
+
