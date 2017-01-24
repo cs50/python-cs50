5472755fae1c23e1588765a5d3b109d45671b0e3
David J. Malan <malan@harvard.edu>
2017-01-24 00:26:54 -0500

    added lazy loading of cs50.SQL, added comments

--- src/sql.py
+++ src/sql.py
@@ -1,19 +1,28 @@
 import sqlalchemy
 
 class SQL(object):
-    """TODO"""
+    """Wrap SQLAlchemy to provide a simple SQL API."""
 
     def __init__(self, url):
-        """TODO"""
+        """
+        Create instance of sqlalchemy.engine.Engine.
+
+        URL should be a string that indicates database dialect and connection arguments.
+
+        http://docs.sqlalchemy.org/en/latest/core/engines.html#sqlalchemy.create_engine
+        """
         try:
             self.engine = sqlalchemy.create_engine(url)
         except Exception as e:
             raise RuntimeError(e)
 
     def execute(self, text, *multiparams, **params):
-        """TODO"""
+        """
+        Execute a SQL statement.
+        """
         try:
 
+            # bind parameters before statement reaches database, so that bound parameters appear in exceptions
             # http://docs.sqlalchemy.org/en/latest/core/sqlelement.html#sqlalchemy.sql.expression.text
             # https://groups.google.com/forum/#!topic/sqlalchemy/FfLwKT1yQlg
             # http://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.Engine.execute
@@ -21,23 +30,23 @@ class SQL(object):
             statement = sqlalchemy.text(text).bindparams(*multiparams, **params)
             result = self.engine.execute(str(statement.compile(compile_kwargs={"literal_binds": True})))
 
-            # SELECT
+            # if SELECT (or INSERT with RETURNING), return result set as list of dict objects
             if result.returns_rows:
                 rows = result.fetchall()
                 return [dict(row) for row in rows]
 
-            # INSERT
+            # if INSERT, return primary key value for a newly inserted row
             elif result.lastrowid is not None:
                 return result.lastrowid
 
-            # DELETE, UPDATE
+            # if DELETE or UPDATE, return number of rows matched
             else:
                 return result.rowcount
 
+        # if constraint violated, return None
         except sqlalchemy.exc.IntegrityError:
             return None
 
+        # else raise error
         except Exception as e:
             raise RuntimeError(e)
-
-
