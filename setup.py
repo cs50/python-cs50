19b33c09cf1e9c0cdf1bfdb303a0495e18539b80
David J. Malan <malan@harvard.edu>
2017-05-20 11:53:24 -0400

    added dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     description="CS50 library for Python",
-    install_requires=["SQLAlchemy"],
+    install_requires=["SQLAlchemy", "sqlparse"],
     keywords="cs50",
     name="cs50",
     packages=["cs50"],
