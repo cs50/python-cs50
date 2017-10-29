c2eb5e583f5e74130378654e70dc0906b91cfc0a
David J. Malan <malan@harvard.edu>
2017-10-29 13:19:47 -0400

    grays out site packages in tracebacks

--- src/cs50/__init__.py
+++ src/cs50/__init__.py
@@ -1,7 +1,11 @@
-import imp
 import sys
+from os.path import abspath, join
+from site import getsitepackages, getusersitepackages
+from termcolor import cprint
+from traceback import extract_tb, format_list, format_exception_only
 
 from .cs50 import *
+
 
 class CustomImporter(object):
     """
@@ -11,14 +15,37 @@ class CustomImporter(object):
     http://xion.org.pl/2012/05/06/hacking-python-imports/
     http://dangerontheranger.blogspot.com/2012/07/how-to-use-sysmetapath-with-python.html
     """
+
     def find_module(self, fullname, path=None):
         if fullname == "cs50.SQL":
             return self
         return None
+
     def load_module(self, name):
         if name in sys.modules:
             return sys.modules[name]
         from .sql import SQL
         sys.modules[name] = SQL
         return SQL
+
+
 sys.meta_path.append(CustomImporter())
+
+
+def excepthook(type, value, tb):
+    """
+    Format traceback, darkening entries from global site-packages and user-specific site-packages directory.
+
+    https://stackoverflow.com/a/33042323/5156190
+    """
+    packages = tuple(join(abspath(p), "") for p in getsitepackages() + [getusersitepackages()])
+    for entry in extract_tb(tb):
+        fmt = format_list((entry,))
+        if (entry.filename.startswith(packages)):
+            cprint("".join(fmt), attrs=["dark"], end="", file=sys.stderr)
+        else:
+            cprint("".join(fmt), end="", file=sys.stderr)
+    cprint("".join(format_exception_only(type, value)), end="")
+
+
+sys.excepthook = excepthook
