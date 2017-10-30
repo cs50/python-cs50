57261ca590e23b23b4685797a03111a7a32dfe41
David J. Malan <malan@harvard.edu>
2017-10-29 22:35:37 -0400

    tidied

--- tests/flask/application.py
+++ tests/flask/application.py
@@ -1,16 +1,13 @@
+import requests
 from flask import Flask, render_template
+
+import cs50
 
 app = Flask(__name__)
 
-import cs50
-import requests
 @app.route("/")
 def index():
     def f():
         res = requests.get("cs50.harvard.edu")
     f()
     return render_template("index.html")
-
-@app.route("/foo", methods=["POST"])
-def foo():
-    return ""
