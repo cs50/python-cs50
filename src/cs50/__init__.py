675c0479cf98b972c44fcbda5deb68f62f2c8e80
David J. Malan <malan@harvard.edu>
2017-12-20 21:39:35 -0500

    wrapping in try/finally

--- src/cs50/__init__.py
+++ src/cs50/__init__.py
@@ -1,47 +1,48 @@
 import os
 import sys
 
-# Save student's sys.path
-path = sys.path[:]
-
-# In case student has files that shadow packages
-sys.path = [p for p in sys.path if p not in ("", os.getcwd())]
-
-# Import cs50_*
-from .cs50 import eprint, get_char, get_float, get_int, get_string
 try:
-    from .cs50 import get_long
-except:
-    pass
 
-# Monkey patches
-from . import flask
+    # Save student's sys.path
+    path = sys.path[:]
 
+    # In case student has files that shadow packages
+    sys.path = [p for p in sys.path if p not in ("", os.getcwd())]
 
-class CustomImporter(object):
-    """
-    Import cs50.SQL lazily so that rest of library can be used without SQLAlchemy installed.
+    # Import cs50_*
+    from .cs50 import eprint, get_char, get_float, get_int, get_string
+    try:
+        from .cs50 import get_long
+    except:
+        pass
 
-    https://docs.python.org/3/library/imp.html
-    http://xion.org.pl/2012/05/06/hacking-python-imports/
-    http://dangerontheranger.blogspot.com/2012/07/how-to-use-sysmetapath-with-python.html
-    """
+    # Replace flask's logger
+    from . import flask
 
-    def find_module(self, fullname, path=None):
-        if fullname == "cs50.SQL":
-            return self
-        return None
+    class CustomImporter(object):
+        """
+        Import cs50.SQL lazily so that rest of library can be used without SQLAlchemy installed.
 
-    def load_module(self, name):
-        if name in sys.modules:
-            return sys.modules[name]
-        from .sql import SQL
-        sys.modules[name] = SQL
-        return SQL
+        https://docs.python.org/3/library/imp.html
+        http://xion.org.pl/2012/05/06/hacking-python-imports/
+        http://dangerontheranger.blogspot.com/2012/07/how-to-use-sysmetapath-with-python.html
+        """
 
+        def find_module(self, fullname, path=None):
+            if fullname == "cs50.SQL":
+                return self
+            return None
 
-sys.meta_path.append(CustomImporter())
+        def load_module(self, name):
+            if name in sys.modules:
+                return sys.modules[name]
+            from .sql import SQL
+            sys.modules[name] = SQL
+            return SQL
 
+    sys.meta_path.append(CustomImporter())
 
-# Restore student's sys.path
-sys.path = path
+finally:
+
+    # Restore student's sys.path (just in case library raised an exception that caller caught)
+    sys.path = path
