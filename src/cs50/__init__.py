f8502d6f3e38a61a498c6b54e7ad71233bebb37f
David J. Malan <malan@harvard.edu>
2018-10-27 19:28:50 -0400

    no longer lazy-loading SQL

--- src/cs50/__init__.py
+++ src/cs50/__init__.py
@@ -1,29 +1,6 @@
 import os
 import sys
 
-
-class CustomImporter(object):
-    """
-    Import cs50.SQL lazily so that rest of library can be used without SQLAlchemy installed.
-
-    https://docs.python.org/3/library/imp.html
-    http://xion.org.pl/2012/05/06/hacking-python-imports/
-    http://dangerontheranger.blogspot.com/2012/07/how-to-use-sysmetapath-with-python.html
-    """
-
-    def find_module(self, fullname, path=None):
-        if fullname == "cs50.SQL":
-            return self
-        return None
-
-    def load_module(self, name):
-        if name in sys.modules:
-            return sys.modules[name]
-        from .sql import SQL
-        sys.modules[name] = SQL
-        return SQL
-
-
 try:
 
     # Save student's sys.path
@@ -42,8 +19,8 @@ try:
     # Replace Flask's logger
     from . import flask
 
-    # Lazily load CS50.SQL
-    sys.meta_path.append(CustomImporter())
+    # Wrap SQLAlchemy
+    from .sql import SQL
 
 finally:
 
