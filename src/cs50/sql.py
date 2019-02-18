8af3fd458ae5a2e1a57d728ae8d8e8adf1780c66
David J. Malan <malan@harvard.edu>
2019-02-17 22:40:57 -0500

    adding support for positional parameters

--- src/cs50/sql.py
+++ src/cs50/sql.py
@@ -25,6 +25,9 @@ class SQL(object):
         http://docs.sqlalchemy.org/en/latest/dialects/index.html
         """
 
+        # Get logger
+        self._logger = logging.getLogger("cs50")
+
         # Require that file already exist for SQLite
         matches = re.search(r"^sqlite:///(.+)$", url)
         if matches:
@@ -41,144 +44,207 @@ class SQL(object):
 
             # Enable foreign key constraints
             if foreign_keys:
-                sqlalchemy.event.listen(self.engine, "connect", _connect)
+                def connect(dbapi_connection, connection_record):
+                    if type(dbapi_connection) is sqlite3.Connection:  # If back end is sqlite
+                        cursor = dbapi_connection.cursor()
+                        cursor.execute("PRAGMA foreign_keys=ON")
+                        cursor.close()
+                sqlalchemy.event.listen(self.engine, "connect", connect)
 
         else:
 
             # Create engine, raising exception if back end's module not installed
             self.engine = sqlalchemy.create_engine(url, **kwargs)
 
-
         # Log statements to standard error
         logging.basicConfig(level=logging.DEBUG)
-        self.logger = logging.getLogger("cs50")
-        disabled = self.logger.disabled
+
+        def parse(self, e):
+            """Parses an exception, returns its message."""
+
+            # MySQL
+            matches = re.search(r"^\(_mysql_exceptions\.OperationalError\) \(\d+, \"(.+)\"\)$", str(e))
+            if matches:
+                return matches.group(1)
+
+            # PostgreSQL
+            matches = re.search(r"^\(psycopg2\.OperationalError\) (.+)$", str(e))
+            if matches:
+                return matches.group(1)
+
+            # SQLite
+            matches = re.search(r"^\(sqlite3\.OperationalError\) (.+)$", str(e))
+            if matches:
+                return matches.group(1)
+
+            # Default
+            return str(e)
 
         # Test database
         try:
-            self.logger.disabled = True
+            disabled = self._logger.disabled
+            self._logger.disabled = True
             self.execute("SELECT 1")
         except sqlalchemy.exc.OperationalError as e:
-            e = RuntimeError(self._parse(e))
+            e = RuntimeError(parse(e))
             e.__cause__ = None
             raise e
         else:
-            self.logger.disabled = disabled
+            self._logger.disabled = disabled
 
-    def _parse(self, e):
-        """Parses an exception, returns its message."""
-
-        # MySQL
-        matches = re.search(r"^\(_mysql_exceptions\.OperationalError\) \(\d+, \"(.+)\"\)$", str(e))
-        if matches:
-            return matches.group(1)
-
-        # PostgreSQL
-        matches = re.search(r"^\(psycopg2\.OperationalError\) (.+)$", str(e))
-        if matches:
-            return matches.group(1)
-
-        # SQLite
-        matches = re.search(r"^\(sqlite3\.OperationalError\) (.+)$", str(e))
-        if matches:
-            return matches.group(1)
-
-        # Default
-        return str(e)
-
-    def execute(self, text, **params):
+    def execute(self, sql, *args, **kwargs):
         """Execute a SQL statement."""
-
-        class UserDefinedType(sqlalchemy.TypeDecorator):
-            """Add support for expandable values, a la https://github.com/sqlalchemy/sqlalchemy/issues/3953."""
-
-            # Required class-level attribute
-            # https://docs.sqlalchemy.org/en/latest/core/custom_types.html#sqlalchemy.types.TypeDecorator
-            impl = sqlalchemy.types.UserDefinedType
-
-            def process_literal_param(self, value, dialect):
-                """Receive a literal parameter value to be rendered inline within a statement."""
-
-                def process(value):
-                    """Render a literal value, escaping as needed."""
-
-                    # bool
-                    if type(value) is bool:
-                        return sqlalchemy.types.Boolean().literal_processor(dialect)(value)
-
-                    # datetime.date
-                    elif type(value) is datetime.date:
-                        return sqlalchemy.types.String().literal_processor(dialect)(value.strftime("%Y-%m-%d"))
-
-                    # datetime.datetime
-                    elif type(value) is datetime.datetime:
-                        return sqlalchemy.types.String().literal_processor(dialect)(value.strftime("%Y-%m-%d %H:%M:%S"))
-
-                    # datetime.time
-                    elif type(value) is datetime.time:
-                        return sqlalchemy.types.String().literal_processor(dialect)(value.strftime("%H:%M:%S"))
-
-                    # float
-                    elif type(value) is float:
-                        return sqlalchemy.types.Float().literal_processor(dialect)(value)
-
-                    # int
-                    elif type(value) is int:
-                        return sqlalchemy.types.Integer().literal_processor(dialect)(value)
-
-                    # long
-                    elif sys.version_info.major != 3 and type(value) is long:
-                        return sqlalchemy.types.Integer().literal_processor(dialect)(value)
-
-                    # str
-                    elif type(value) is str:
-                        return sqlalchemy.types.String().literal_processor(dialect)(value)
-
-                    # None
-                    elif type(value) is sqlalchemy.sql.elements.Null:
-                        return sqlalchemy.types.NullType().literal_processor(dialect)(value)
-
-                    # Unsupported value
-                    raise RuntimeError("unsupported value")
-
-                # Process value(s), separating with commas as needed
-                if type(value) is list:
-                    return ", ".join([process(v) for v in value])
-                else:
-                    return process(value)
 
         # Allow only one statement at a time, since SQLite doesn't support multiple
         # https://docs.python.org/3/library/sqlite3.html#sqlite3.Cursor.execute
-        if len(sqlparse.split(text)) > 1:
+        statements = sqlparse.parse(sql)
+        if len(statements) > 1:
             raise RuntimeError("too many statements at once")
+        elif len(statements) == 0:
+            raise RuntimeError("missing statement")
+
+        # Ensure named and positional parameters are mutually exclusive
+        if len(args) > 0 and len(kwargs) > 0:
+            raise RuntimeError("cannot pass both named and positional parameters")
+
+        # In case user passes args in list or tuple
+        if len(args) == 1 and (isinstance(args[0], list) or isinstance(args[0], tuple)):
+            args = args[0]
+
+        # In case user passes kwargs in dict
+        if len(args) == 1 and len(kwargs) == 0 and isinstance(args[0], dict):
+            kwargs = args[0]
+
+        # Flatten statement
+        tokens = list(statements[0].flatten())
+
+        # Validate paramstyle
+        placeholders = {}
+        paramstyle = None
+        for index, token in enumerate(tokens):
+
+            # If token is a placeholder
+            if token.ttype == sqlparse.tokens.Name.Placeholder:
+
+                # Determine paramstyle
+                if token.value == "?":
+                    _paramstyle = "qmark"
+                elif re.search(r"^:[1-9]\d*$", token.value):
+                    _paramstyle = "numeric"
+                elif re.search(r"^:[a-zA-Z]\w*$", token.value):
+                    _paramstyle = "named"
+                elif re.search(r"^TODO$", token.value):  # TODO
+                    _paramstyle = "named"
+                elif re.search(r"%\([a-zA-Z]\w*\)s$", token.value):  # TODO
+                    _paramstyle = "pyformat"
+                else:
+                    raise RuntimeError("{}: invalid placeholder".format(token.value))
+
+                # Ensure paramstyle is consistent
+                if paramstyle is not None and _paramstyle != paramstyle:
+                    raise RuntimeError("inconsistent paramstyle")
+
+                # Remember paramstyle
+                if paramstyle is None:
+                    paramstyle = _paramstyle
+
+                # Remember placeholder
+                placeholders[index] = token.value
+
+        def escape(value):
+
+            # bool
+            if type(value) is bool:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.Number,
+                    sqlalchemy.types.Boolean().literal_processor(self.engine.dialect)(value))
+
+            # datetime.date
+            elif type(value) is datetime.date:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.String,
+                    sqlalchemy.types.String().literal_processor(self.engine.dialect)(value.strftime("%Y-%m-%d")))
+
+            # datetime.datetime
+            elif type(value) is datetime.datetime:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.String,
+                    sqlalchemy.types.String().literal_processor(self.engine.dialect)(value.strftime("%Y-%m-%d %H:%M:%S")))
+
+            # datetime.time
+            elif type(value) is datetime.time:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.String,
+                    sqlalchemy.types.String().literal_processor(self.engine.dialect)(value.strftime("%H:%M:%S")))
+
+            # float
+            elif type(value) is float:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.Number,
+                    sqlalchemy.types.Float().literal_processor(self.engine.dialect)(value))
+
+            # int
+            elif type(value) is int:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.Number,
+                    sqlalchemy.types.Integer().literal_processor(self.engine.dialect)(value))
+
+            # str
+            elif type(value) is str:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.String,
+                    sqlalchemy.types.String().literal_processor(self.engine.dialect)(value))
+
+            # None
+            elif value is None:
+                return sqlparse.sql.Token(
+                    sqlparse.tokens.Keyword,
+                    sqlalchemy.types.NullType().literal_processor(self.engine.dialect)(value))
+
+            # Unsupported value
+            raise RuntimeError("unsupported value: {}".format(value))
+
+        # qmark
+        if paramstyle == "qmark":
+
+            # Validate number of placeholders
+            if len(placeholders) < len(args):
+                raise RuntimeError("too few placeholders")
+            elif len(placeholders) > len(args):
+                raise RuntimeError("too many placeholders")
+
+            # Escape values
+            for i, index in enumerate(placeholders.keys()):
+                tokens[index] = escape(args[i])
+
+        # numeric
+        elif paramstyle == "numeric":
+
+            # Escape values
+            for index, value in placeholders.items():
+                i = int(re.sub(r"^:", "", value)) - 1
+                if i >= len(args):
+                    raise RuntimeError("placeholder out of range")
+                tokens[index] = escape(args[i])
+
+        # named
+        elif paramstyle == "named":
+
+            # Escape values
+            for index, value in placeholders.items():
+                name = re.sub(r"^:", "", value)
+                if name not in kwargs:
+                    raise RuntimeError("missing value for placeholder")
+                tokens[index] = escape(kwargs[name])
+
+        # Join tokens into statement
+        statement = "".join([str(token) for token in tokens])
 
         # Raise exceptions for warnings
         warnings.filterwarnings("error")
 
         # Prepare, execute statement
         try:
-
-            # Construct a new TextClause clause
-            statement = sqlalchemy.text(text)
-
-            # Iterate over parameters
-            for key, value in params.items():
-
-                # Translate None to NULL
-                if value is None:
-                    value = sqlalchemy.sql.null()
-
-                # Bind parameters before statement reaches database, so that bound parameters appear in exceptions
-                # http://docs.sqlalchemy.org/en/latest/core/sqlelement.html#sqlalchemy.sql.expression.text
-                statement = statement.bindparams(sqlalchemy.bindparam(
-                    key, value=value, type_=UserDefinedType()))
-
-            # Stringify bound parameters
-            # http://docs.sqlalchemy.org/en/latest/faq/sqlexpressions.html#how-do-i-render-sql-expressions-as-strings-possibly-with-bound-parameters-inlined
-            statement = str(statement.compile(compile_kwargs={"literal_binds": True}))
-
-            # Statement for logging
-            log = re.sub(r"\n\s*", " ", sqlparse.format(statement, reindent=True))
 
             # Execute statement
             result = self.engine.execute(statement)
@@ -213,30 +279,17 @@ class SQL(object):
 
         # If constraint violated, return None
         except sqlalchemy.exc.IntegrityError:
-            self.logger.debug(termcolor.colored(log, "yellow"))
+            self._logger.debug(termcolor.colored(statement, "yellow"))
             return None
 
         # If user errror
         except sqlalchemy.exc.OperationalError as e:
-            self.logger.debug(termcolor.colored(log, "red"))
+            self._logger.debug(termcolor.colored(statement, "red"))
             e = RuntimeError(self._parse(e))
             e.__cause__ = None
             raise e
 
         # Return value
         else:
-            self.logger.debug(termcolor.colored(log, "green"))
+            self._logger.debug(termcolor.colored(statement, "green"))
             return ret
-
-
-# http://docs.sqlalchemy.org/en/latest/dialects/sqlite.html#foreign-key-support
-def _connect(dbapi_connection, connection_record):
-    """Enables foreign key support."""
-
-    # If back end is sqlite
-    if type(dbapi_connection) is sqlite3.Connection:
-
-        # Respect foreign key constraints by default
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON")
-        cursor.close()
