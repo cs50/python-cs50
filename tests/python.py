7d82131e29930cc6fd71f963a11968b5897b745d
Sudipto Ghosh <sudipto@ghosh.pro>
2019-10-12 13:20:55 +0530

    added tests in python.py

--- tests/python.py
+++ tests/python.py
@@ -4,5 +4,29 @@ sys.path.insert(0, "../src")
 
 import cs50
 
-i = cs50.get_int()
-print(i)
+def test_get_int():
+    """
+    Tests the get_int() function
+    """
+    i = cs50.get_int("Integer: ")
+    assert type(i) == type(0), "get_int() did not return an integer"
+
+def test_get_float():
+    """
+    Tests the get_ifloat() function
+    """
+    i = cs50.get_float("Float: ")
+    assert type(i) == type(0.0), "get_float() did not return a float"
+
+def test_get_string():
+    """
+    Tests the get_string() function
+    """
+    i = cs50.get_string("String: ")
+    assert type(i) == type(""), "get_string() did not return a string"
+
+if __name__ == "__main__":
+    test_get_int()
+    test_get_float()
+    test_get_string()
+    print("All tests passed!")
