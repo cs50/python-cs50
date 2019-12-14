bb17dd917b57666bd33834c328a97180409008c4
David J. Malan <malan@harvard.edu>
2019-12-14 08:35:03 -0500

    coercing memoryview to bytes

--- src/cs50/sql.py
+++ src/cs50/sql.py
@@ -280,13 +280,21 @@ class SQL(object):
                     # If SELECT, return result set as list of dict objects
                     if value == "SELECT":
 
-                        # Coerce any decimal.Decimal objects to float objects
-                        # https://groups.google.com/d/msg/sqlalchemy/0qXMYJvq8SA/oqtvMD9Uw-kJ
+                        # Coerce types
                         rows = [dict(row) for row in result.fetchall()]
                         for row in rows:
                             for column in row:
+
+                                # Coerce decimal.Decimal objects to float objects
+                                # https://groups.google.com/d/msg/sqlalchemy/0qXMYJvq8SA/oqtvMD9Uw-kJ
                                 if type(row[column]) is decimal.Decimal:
                                     row[column] = float(row[column])
+
+                                # Coerce memoryview objects (as from PostgreSQL's bytea columns) to bytes
+                                elif type(row[column]) is memoryview:
+                                    row[column] = bytes(row[column])
+
+                        # Rows to be returned
                         ret = rows
 
                     # If INSERT, return primary key value for a newly inserted row
