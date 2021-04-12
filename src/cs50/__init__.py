a4989eb05e078f78ac5dc488300b804ae3ecb996
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-04-12 19:37:49 -0400

    wrap flask wrapper
    the IDE proxy now handles forcing https

--- src/cs50/__init__.py
+++ src/cs50/__init__.py
@@ -1,9 +1,7 @@
-"""Exposes API, wraps flask, and sets up logging"""
+"""Exposes API and sets up logging"""
 
 from .cs50 import get_float, get_int, get_string
 from .sql import SQL
 from ._logger import _setup_logger
-from ._flask import _wrap_flask
 
 _setup_logger()
-_wrap_flask()
