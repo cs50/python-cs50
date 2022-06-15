0039f7e2ba76f561ac5eaf93d4f63d160c660464
David J. Malan <malan@harvard.edu>
2022-06-14 20:26:16 -0400

    fixed license

--- setup.py
+++ setup.py
@@ -12,9 +12,11 @@ setup(
     description="CS50 library for Python",
     install_requires=["Flask>=1.0", "SQLAlchemy", "sqlparse", "termcolor", "wheel"],
     keywords="cs50",
+    license="GPLv3",
+    long_description_content_type="text/markdown",
     name="cs50",
     package_dir={"": "src"},
     packages=["cs50"],
     url="https://github.com/cs50/python-cs50",
-    version="9.2.1"
+    version="9.2.2"
 )
